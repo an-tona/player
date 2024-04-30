93d2b4599ac8aad3b7baff19e02b98f842b01853
Anton Bilashuk <antonbilashuk@gmail.com>
2024-04-30 13:46:52 +0200

    second build

--- src/visual_components/MainPage.js
+++ src/visual_components/MainPage.js
@@ -5,10 +5,6 @@ import preloader from '../images/preloader.gif';
 function MainPage() {
   return (
     <>
-    <div className='track_search_preloader_container' style={{display: 'flex', justifyContent:'center', alignItems:'center'}}>
-            <h4>Searching tracks...</h4>
-            <img src={preloader} alt='preloader' style={{width:'30px', height:'30px'}}/>
-        </div> 
     </>
    
   )
